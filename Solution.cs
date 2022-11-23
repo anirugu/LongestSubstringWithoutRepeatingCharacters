140e44c2eeaebf027a3e5d67113244b7a55d1a9e
Anirudha Gupta <anirudhagupta01@gmail.com>
2022-11-23 10:45:29 +0530

    initial commit

---
+++ Solution.cs
@@ -0,0 +1,19 @@
+﻿class Solution
+{
+    public int lengthOfLongestSubstring(String s)
+    {
+       HashSet<char> vs = new HashSet<char>();
+        HashSet<int> v2 = new HashSet<int>();
+
+        for(int i = 0; i < s.Length; i++)
+        {
+           if(vs.Contains(s[i]))
+                v2.Add(i);
+            else
+            {
+                vs.Add(s[i]);
+            }
+        }
+        return v2.Count;
+    }
+}
